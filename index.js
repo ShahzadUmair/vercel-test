27e02a7e5dd49e8af9d0b2de7e44dc64688e2768
UmairShahzad <18100099@lums.edu.pk>
2020-08-15 03:55:53 +0500

    cat cpu info

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   try {
-    const { stdout, stderr } = await exec("cd /proc && ls");
+    const { stdout, stderr } = await exec("cd /proc && cat cpuinfo");
     res.send(stdout);
   } catch (err) {
     res.send(err);
